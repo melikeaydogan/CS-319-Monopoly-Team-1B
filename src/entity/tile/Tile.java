b14163ccdacdf192a8db90ffe05248559fff0a2b
melikeaydogan <melike.aydo99@gmail.com>
2020-11-28 20:04:57 +0300

    Deserializer for JSON is added.

--- src/entity/tile/Tile.java
+++ src/entity/tile/Tile.java
@@ -5,7 +5,15 @@ import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParseException;
 import entity.property.Building;
-import entity.property.Dorm;
+//import entity.property.Dorm;
+import entity.property.Facility;
+import entity.property.Property;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import entity.property.Building;
 import entity.property.Facility;
 import entity.property.Property;
 
@@ -14,21 +22,14 @@ import java.lang.reflect.Type;
 
 public class Tile {
 
-    private final Image image;
     private final int tileId;
 
     public Tile() {
-        image = null;
         tileId = 0;
     }
 
-    public Tile(Image image, int tileId) {
-        this.image = image;
+    public Tile( int tileId) {
         this.tileId = tileId;
-    }
-
-    public Image getImage() {
-        return image;
     }
 
     public int getTileId() {
@@ -63,4 +64,33 @@ public class Tile {
 
     }
 
+    public static class CustomDeserializer implements JsonDeserializer<Tile> {
+
+        public Tile deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+
+            if (json == null)
+                return null;
+            else {
+                String type = json.getAsJsonObject().get("type").getAsString();
+                switch(type){
+                    case "START":
+                        return context.deserialize(json, StartTile.class);
+                    case "PROPERTY":
+                        return context.deserialize(json, PropertyTile.class);
+                    case "CARD":
+                        return context.deserialize(json, CardTile.class);
+                    case "TAX":
+                        return context.deserialize(json, TaxTile.class);
+                    case "JAIL":
+                        return context.deserialize(json, JailTile.class);
+                    case "FREE":
+                        return context.deserialize(json, FreeParkingTile.class);
+                    default:
+                        return null;
+                }
+            }
+
+        }
+
+    }
 }
